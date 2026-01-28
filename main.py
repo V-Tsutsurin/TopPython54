48c5cc038c8e05a966017afe1ecd8ac199e6fbc0
V.Tsutsurin <v.tsutsurin@yandex.ru>
2026-01-28 21:17:59 +0300

    27.01.2026

---
+++ main.py
@@ -0,0 +1,467 @@
+# python -m venv .env
+# .\.env\Scripts\activate
+
+# name_new = "Иван"
+# name = "Антон"
+# age = 20
+# print('Hello ' + name_new + ". I am " + str(age))
+#
+# print(type(name))
+# print(type(age))
+# print(id(name))
+# print(id(age))
+#
+# a = b = c = 1
+# print(a, b, c)
+#
+# a, b, c = 5, "Hello", 9.2
+# print(a, b, c)
+# import keyword
+# print(keyword.kwlist)
+#
+# a = 5
+# print(a)
+# a = 6
+# print(a)
+#
+# PI = 3.14
+# print(PI)
+# PI = 2
+# print(PI)
+#
+# a = 1
+# b = 2
+# print("a:", a)
+# print("b:", b)
+# # c = a  # c = 1
+# # a = b  # a = 2
+# # b = c  # b = 1
+# a, b = b, a
+# print("a:", a)
+# print("b:", b)
+#
+# print("строка \nТекстовые последовательности или строки (string) – это набор символов, заключенный в кавычки.\nТекстовые последовательности или строки (string) – это набор символов, заключенный в кавычки. символов")
+# print('строка \
+#       символов')
+#
+# print("Документ \"script.py\" находтся \rпо заданному пути: \n\tD:\\Python\\project")
+#
+# print(type(46460))
+# print(type(4.44564156))
+# print(445641564454156341563416548674874896749)
+# print(4.445641564454156341563416548674874896749)
+#
+# print(6 + 2)
+# print(6 - 2)
+# print(6 * 2)
+# print(5 / 2)
+# print(5 // 2)
+# print(5 ** 2)
+# print(5 % 2)
+#
+# a = 5
+# b = 7
+# c = 3
+# s = a + b + c
+# print("Сумма:", s)
+# print("Произведение:", a * b * c)
+# print("Среднее арифметическое:", s / 3)
+#
+# number = 6 + 4 * 5 ** 2 + 7
+# print(number)
+# number = (6 + 4) * (5 ** 2 + 7)
+# print(number)
+#
+# num = 4321  # 432
+# print("Исходное число:", num)
+# a = num % 10
+# print(a)
+# num = num // 10
+# # print(num)
+# b = num % 10
+# print(b)
+# num = num // 10
+# # print(num)
+# c = num % 10
+# print(c)
+# num = num // 10
+# # print(num)
+# d = num % 10
+# print(d)
+# print(a * 1000 + b * 100 + c * 10 + d)
+#
+# num = 9573  # 432
+# print("Исходное число:", num)
+# res = (num % 10) * 1000
+# num = num // 10  # 432
+# res += (num % 10) * 100
+# num = num // 10
+# res += (num % 10) * 10
+# num = num // 10
+# res += num % 10
+# print(res)
+#
+# a = int(input("Введите число: "))
+# # a = int(a)
+# print(type(a))
+# print(a * 2)
+#
+# a = 10
+# b = 2
+# print("a:", a)
+# print("b:", b)
+# a = a + b  # a = 10 + 2 = 12
+# b = a - b  # b = 12 - 2 = 10
+# a = a - b  # a = 12 - 10 = 2
+# print("a:", a)
+# print("b:", b)
+#
+# # Функции преобразования типов
+# # int() - числовой тип
+# # str() - строковый тип
+# # float() - вещественный тип
+# # bool() - булевый тип
+#
+# num1 = "2"
+# num2 = 3
+# res = int(num1) + num2
+# # res = num1 + str(num2)
+# print(res)
+# print(type(res))
+#
+# print(int(3.8))
+# print(round(3.8954144165341, 5))
+#
+# print(5 / 2)
+#
+# a = '5.2'
+# # b = 10
+# # c = float(a) + b
+# # print(c)
+# # print(type(c))
+#
+# name = "Виктор"
+# age = 28
+# print("Меня зовут", name, ". Мне", age, "лет.")
+# print("Меня зовут " + name + ". Мне " + str(age) + " лет.")
+# print("Меня зовут ", name, ". Мне ", age, " лет.", sep="---", end=" ")
+# print("Я учу Python")
+#
+# num = int(input("Введите число: "))
+# power = int(input("Введите степень: "))
+# res = num ** power
+# print("Число", num, "в степень", power, "равно:", res)
+#
+# b1 = True
+# # b2 = False
+# # print(b1 + 5)
+# # print(b2 + 5)
+# print(type(b1))
+#
+# print(bool("python"))
+# print(bool(""))  # False
+# print(bool(" "))
+# print(bool(-15.2))
+# print(bool(0))  # False
+# print(bool(False))  # False
+# print(bool(None))  # False
+#
+# test = None
+# print(test)
+# print(type(test))
+# test = 5
+# print(test)
+# print(type(test))
+#
+# print(7 == 7)
+# print(7 + 2 == 7)
+# print(7 + 2 != 7)
+# print(8 > 5)
+# print(8 < 5)
+# print(8 >= 8)
+# print(8 <= 5)
+# print('привет' > 'Привет')
+#
+#
+# print(2 < 14 < 9)
+# print(2 * 5 > 7 > 4 + 3)
+#
+# a = 10
+# b = 5
+# c = a == b
+# print(a, b, c)
+#
+# print(5 - 3 == 2 and 1 + 3 == 4)  # True (True : True)
+# print(5 - 3 == 2 and 1 + 3 < 4)  # False (True : False)
+# print(5 - 3 > 2 and 1 + 3 == 4)  # False (False : True)
+# print(5 - 3 > 2 and 1 + 3 < 4)  # False (False : False)
+#
+#
+# print(5 - 3 == 2 or 1 + 3 == 4)  # True (True : True)
+# print(5 - 3 == 2 or 1 + 3 < 4)  # True (True : False)
+# print(5 - 3 > 2 or 1 + 3 == 4)  # True (False : True)
+# print(5 - 3 > 2 or 1 + 3 < 4)  # False (False : False)
+#
+#
+# print(not 9 - 5)
+# print(not 5 - 5)
+#
+# cnt = 15
+# if cnt < 10:
+#     cnt += 1
+# print(cnt)
+#
+# age = int(input("Введите свой возраст: "))
+# if age >= 18:
+#     print("Доступ на сайт разрешен")
+# else:
+#     print("Доступ запрещен")
+#
+# a = 15
+# b = 5
+# # if a > b:
+# #     print("a > b")
+# # elif a < b:
+# #     print("b > a")
+# # else:
+# #     print("b == a")
+#
+# if a > b:
+#     print("a > b")
+# if a < b:
+#     print("b > a")
+# if a == b:
+#     print("b == a")
+#
+# a = input("Введите первую сторону: ")
+# b = input("Введите вторую сторону: ")
+# c = input("Введите третью сторону: ")
+#
+# # if a == b and b == c and c == a:
+# if a == b == c:
+#     print('Треугольник равносторонний')
+# elif a == b or c == a or b == c:
+#     print('Треугольник равнобедренный')
+# # elif not (a == b and b == c and c == a):
+# # elif a != b and b != c and c != a:
+# else:
+#     print("Треугольник разносторнний")
+#
+# num1 = 10
+# num2 = 20
+# num3 = 20
+# if num1 == num2 == num3:
+#     print("Равносторонний")
+# elif num1 == num2 or num1 == num3 or num2 == num3:
+#     print("Равнобедренный")
+# else:
+#     print("Разносторонний")
+#
+# day = int(input("Введите день недели (цифрой): "))
+# if 1 <= day <= 5:  # (day >= 1) and (day <= 5):
+#     print("Рабочий день - ", end="")
+#     if day == 1:
+#         print("понедельник")
+#     if day == 2:
+#         print("вторник")
+#     if day == 3:
+#         print("среда")
+#     if day == 4:
+#         print("четверг")
+#     if day == 5:
+#         print("пятница")
+# elif day == 6 or day == 7:
+#     print("Выходной день - ", end="")
+#     if day == 6:
+#         print("суббота")
+#     if day == 7:
+#         print("воскресенье")
+# else:
+#     print("Такого дня недели не существует!")
+#
+# m = int(input("Введите номер месяца: "))
+# month = int(input('Введите номер месяца: '))
+# if month == 1 or month == 2 or month == 12:
+#     print('Зима')
+# elif month == 3 or month == 4 or month == 5:
+#     print('Весна')
+# elif month == 6 or month == 7 or month == 8:
+#     print('Лето')
+# elif month == 9 or month == 10 or month == 11:
+#     print('Осень')
+# else:
+#     print('Ошибка')
+#
+# month = int(input('Введите номер месяца: '))
+# if 1 <= month <= 12:
+#     if 3 <= month <= 5:
+#         print('Весна')
+#     elif 6 <= month <= 8:
+#         print('Лето')
+#     elif 9 <= month <= 11:
+#         print('Осень')
+#     else:
+#         print('Зима')
+# else:
+#     print("Ошибка ввода данных")
+#
+#
+# n = int(input("Введите количество ворон: "))
+# if 0 <= n <= 9:
+#     if n == 1:
+#         print("На ветке", n, "ворона")
+#     if 2 <= n <= 4:
+#         print("На ветке", n, "вороны")
+#     # else:
+#     if 5 <= n <= 9 or n == 0:
+#         print("На ветке", n, "ворон")
+# else:
+#     print("Ошибка ввода данных")
+#
+# # (условие ? True : False)
+#
+# a, b = 10, 20
+#
+# minim = "a == b" if a == b else "a > b" if a > b else "b > a"
+# print(minim)
+#
+#
+# a = 5
+# b = 0
+# print(a / b)
+#
+# try:
+#     n = int(input("Введите делимое: "))
+#     m = int(input("Введите делитель: "))
+#     print(n / m)
+# except (ValueError, ZeroDivisionError):
+#     print("Неправильные данные")
+# else:
+#     print("Все нормально. Вы ввели числа", n, "и", m)
+# finally:
+#     print("Конец программы")
+# # except ValueError:
+# #     print("Нельзя вводить строки")
+# # except ZeroDivisionError:
+# #     print("Нельзя делить на ноль")
+#
+# print("OK!!!")
+#
+# try:
+#     a = int(input('Введите первое значение: '))
+#     b = int(input('Введите второе значение: '))
+#     print(a + b)
+# except ValueError:
+#     print(str(a) + str(b))
+#
+#
+# a = input('Введите первое число: ')
+# b = input('Введите второе: ')
+# try:
+#     a = int(a)  # a = 2
+#     b = int(b)  # b = пять
+# except ValueError:
+#     a = str(a)
+#     b = b
+# finally:
+#     print(a + b)
+#
+#
+# # while условие:
+# #     блок инструкций
+#
+# i = 10
+# while i >= 0:
+#     print("i =", i)
+#     i -= 1  # i = i + 1
+#
+# i = 1
+# while i <= 20:
+#     if i % 2:
+#         print(i, end=" ")
+#     i += 1
+#
+# i = 1000
+# while i >= 1:
+#     print(i, end=" ")
+#     i //= 10
+#
+# n = int(input("Укажите количество символов: "))
+# i = 0
+# while i < n:
+#     print("*", end="")
+#     i += 1
+#
+# n = int(input("Укажите количество символов: "))
+# while n > 0:
+#     print("*", end="")
+#     n -= 1
+#
+#
+# n = int(input('Введите число: '))  # 1
+# m = int(input('Введите число: '))  # 5
+# # i = n
+# res = 0
+# while n <= m:
+#     if n % 2:
+#         res += n  # res = res + n
+#         print(n, end=" ")
+#     n += 1
+# print("Сумма целых нечетных чисел:", res)
+#
+#
+# n = input("Введите целое число: ")
+#
+# while type(n) != int:
+#     try:
+#         n = int(n)
+#     except ValueError:
+#         print("Число не целое")
+#         n = input("Введите целое число: ")
+#
+# if n % 2 == 0:
+#     print("Четное")
+# else:
+#     print("Нечетное")
+#
+# i = 0
+# while i < 10:
+#     if i == 3:
+#         i += 1
+#         continue
+#     print(i, end=" ")
+#     if i == 5:
+#         break
+#     i += 1
+# print("\nЦикл завершен!")
+#
+# i = 0
+# while True:
+#     print(i)
+#     if i == 5:
+#         break
+#     i += 1
+#
+# while True:
+#     n = int(input("Введите положительное целое число: "))
+#     if not n < 0:
+#         break
+#
+# print(n)
+# res = 1
+# while True:
+#     n = int(input('Введите число: '))
+#     if n == 0:
+#         break
+#     res *= n
+# print("Результат:", res)
+#
+#
+# i = 0
+# while i < 10:
+#     if i == 5:
+#         break
+#     print(i)
+#     i += 1
+# else:
+#     print("Цикл окончен, i =", i)
---
+++ readme.md
@@ -0,0 +1,55 @@
+# Заголовк 1 уровня
+## Заголовк 2 уровня
+### Заголовк 3 уровня
+#### Заголовк 4 уровня
+##### Заголовк 5 уровня
+
+Текст *курсивом*
+
+Текст **жирный**
+
+Текст ***жирный-курсив*** и ___жирный-курсив___
+
+Текст ~~зачеркнутый~~
+
+- Пункт 1
+- Пункт 2
+    - Под пункт 1
+    - Под пункт 2
+
+
+1. Пункт 1
+2. Пункт 2
+
+[текст](https://media.tproger.ru/uploads/2024/10/cd29a15c-7fcd-4fc5-b8b0-c6860411667c.png)
+
+
+```js
+const = 0
+```
+
+```html
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+```
+***
+---
+___
+
+
+|Имя|Возрат|Город|
+|---:|:---|:--:|
+|**И**|Возрат|Г|
+|Имя|т|Город|
+
+
+![picture all](https://media.tproger.ru/uploads/2024/10/cd29a15c-7fcd-4fc5-b8b0-c6860411667c.png)
