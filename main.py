4f029faf166e14c9e043b411144903b2eee7592d
MaximBytecamp <mmaxim01@yandex.ru>
2026-05-26 21:58:11 +0300

    26.05.2026

--- main.py
+++ main.py
@@ -6451,126 +6451,191 @@
 #
 # for i in range(5):
 #     write_json(gen_person()[0], gen_person()[1])
+#
+# import json
+#
+#
+# class Student:
+#     def __init__(self, name, marks):
+#         self.name = name
+#         self.marks = marks
+#
+#     def __str__(self):
+#         a = ''
+#         for i in self.marks:
+#             a += str(i) + ", "
+#         return f'Студент: {self.name} {a[:-2]}'
+#
+#     def add_mark(self, mark):
+#         self.marks.append(mark)
+#
+#     def delete_mark(self, index):
+#         self.marks.pop(index)
+#
+#     def edit_mark(self, index, new_mark):
+#         self.marks[index] = new_mark
+#
+#     def average_mark(self):
+#         return round(sum(self.marks) / len(self.marks), 2)
+#
+#     @classmethod
+#     def dump_to_json(cls, stud, filename):
+#         try:
+#             data = json.load(open(filename))
+#         except FileNotFoundError:
+#             data = []
+#
+#         data.append({"name": stud.name, "marks": stud.marks})
+#         with open(filename, "w") as f:
+#             json.dump(data, f, indent=2)
+#
+#     @classmethod
+#     def load_from_file(cls, filename):
+#         with open(filename, "r") as f:
+#             print(json.load(f))
+#
+#
+# class Group:
+#     def __init__(self, students, group):
+#         self.students = students
+#         self.group = group
+#
+#     def __str__(self):
+#         a = ""
+#         for i in self.students:
+#             a += str(i) + "\n"
+#         return f"Группа: {self.group}\n{a}"
+#
+#     def add_student(self, student):
+#         self.students.append(student)
+#
+#     def remove_student(self, index):
+#         return self.students.pop(index)
+#
+#     @classmethod
+#     def change_group(cls, group1, group2, index):
+#         return group2.add_student(group1.remove_student(index))
+#
+#     @classmethod
+#     def dump_group(cls, file, group):
+#         try:
+#             data = json.load(open(file))
+#         except FileNotFoundError:
+#             data = []
+#
+#         with open(file, 'w') as f:
+#             stud_list = []
+#             for i in group.students:
+#                 stud_list.append([i.name, i.marks])
+#             data.append(stud_list)
+#             json.dump(data, f, indent=2)
+#
+#     @classmethod
+#     def upload_group(cls, file):
+#         with open(file, "r") as f:
+#             print(json.load(f))
+#
+#
+#
+# st1 = Student('Petrov', [5, 1, 4, 3, 4, 5, 2])
+# st2 = Student('Ivanov', [3, 4, 2, 5, 3, 2, 3])
+# st3 = Student('Kosheva', [5, 4, 5, 4, 2, 5, 4, 5])
+# st4 = Student('Ignatova', [2, 3, 4, 5, 5, 4, 3, 2])
+# sts = [st1, st2]
+#
+# # Student.dump_to_json(st1, "student.json")
+# # Student.dump_to_json(st2, "student.json")
+# # Student.load_from_file("student.json")
+#
+# my_group = Group(sts, "Python-54")
+# # print(my_group)
+# # print('*' * 30)
+# # my_group.add_student(st3)
+# # print(my_group)
+# # # my_group.remove_student(1)
+# # # print(my_group)
+# #
+# # sts2 = [st4]
+# # my_group2 = Group(sts2, "Python-66")
+# # print(my_group2)
+# #
+# # Group.change_group(my_group, my_group2, 1)
+# # print(my_group)
+# # print(my_group2)
+# # Group.dump_group('group.json', my_group)
+# Group.upload_group('group.json')
+#
+# #
+# # # print(st1)
+# # # st1.add_mark(5)
+# # # print(st1)
+# # # st1.delete_mark(1)
+# # # print(st1)
+# # # st1.edit_mark(5, 4)
+# # # print(st1)
+# # # print(st1.average_mark())
 
+# API
+
+import requests
 import json
 
+response = requests.get("https://jsonplaceholder.typicode.com/todos")
+todos = json.loads(response.text)
 
-class Student:
-    def __init__(self, name, marks):
-        self.name = name
-        self.marks = marks
+# print(todos[:10])
+# print(type(todos))
 
-    def __str__(self):
-        a = ''
-        for i in self.marks:
-            a += str(i) + ", "
-        return f'Студент: {self.name} {a[:-2]}'
-
-    def add_mark(self, mark):
-        self.marks.append(mark)
-
-    def delete_mark(self, index):
-        self.marks.pop(index)
-
-    def edit_mark(self, index, new_mark):
-        self.marks[index] = new_mark
-
-    def average_mark(self):
-        return round(sum(self.marks) / len(self.marks), 2)
-
-    @classmethod
-    def dump_to_json(cls, stud, filename):
-        try:
-            data = json.load(open(filename))
-        except FileNotFoundError:
-            data = []
-
-        data.append({"name": stud.name, "marks": stud.marks})
-        with open(filename, "w") as f:
-            json.dump(data, f, indent=2)
-
-    @classmethod
-    def load_from_file(cls, filename):
-        with open(filename, "r") as f:
-            print(json.load(f))
-
-
-class Group:
-    def __init__(self, students, group):
-        self.students = students
-        self.group = group
-
-    def __str__(self):
-        a = ""
-        for i in self.students:
-            a += str(i) + "\n"
-        return f"Группа: {self.group}\n{a}"
-
-    def add_student(self, student):
-        self.students.append(student)
-
-    def remove_student(self, index):
-        return self.students.pop(index)
-
-    @classmethod
-    def change_group(cls, group1, group2, index):
-        return group2.add_student(group1.remove_student(index))
-
-    @classmethod
-    def dump_group(cls, file, group):
-        try:
-            data = json.load(open(file))
-        except FileNotFoundError:
-            data = []
-
-        with open(file, 'w') as f:
-            stud_list = []
-            for i in group.students:
-                stud_list.append([i.name, i.marks])
-            data.append(stud_list)
-            json.dump(data, f, indent=2)
-
-    @classmethod
-    def upload_group(cls, file):
-        with open(file, "r") as f:
-            print(json.load(f))
-
-
-
-st1 = Student('Petrov', [5, 1, 4, 3, 4, 5, 2])
-st2 = Student('Ivanov', [3, 4, 2, 5, 3, 2, 3])
-st3 = Student('Kosheva', [5, 4, 5, 4, 2, 5, 4, 5])
-st4 = Student('Ignatova', [2, 3, 4, 5, 5, 4, 3, 2])
-sts = [st1, st2]
-
-# Student.dump_to_json(st1, "student.json")
-# Student.dump_to_json(st2, "student.json")
-# Student.load_from_file("student.json")
-
-my_group = Group(sts, "Python-54")
-# print(my_group)
-# print('*' * 30)
-# my_group.add_student(st3)
-# print(my_group)
-# # my_group.remove_student(1)
-# # print(my_group)
+# todos_by_user = {}
 #
-# sts2 = [st4]
-# my_group2 = Group(sts2, "Python-66")
-# print(my_group2)
+# for todo in todos:
+#     if todo["completed"]:
+#         try:
+#             todos_by_user[todo['userId']] += 1
+#         except KeyError:
+#             todos_by_user[todo['userId']] = 1
+# # print(todos_by_user)
 #
-# Group.change_group(my_group, my_group2, 1)
-# print(my_group)
-# print(my_group2)
-# Group.dump_group('group.json', my_group)
-Group.upload_group('group.json')
+# top_user = sorted(todos_by_user.items(), key=lambda x: x[1], reverse=True)
+# # print(top_user)
+#
+# max_completed = top_user[0][1]
+# # print(max_completed)
+#
+# users = []
+# for user, num_completed in top_user:
+#     if num_completed < max_completed:
+#         break
+#     users.append(str(user))
+#
+# # print(users)
+# max_user = " and ".join(users)
+#
+# s = "s" if len(users) > 1 else ""
+# print(f"user{s} {max_user} completed {max_completed} TODOs")
+#
+#
+# def keep(todo):
+#     is_completed = todo["completed"]
+#     has_max_count = str(todo["userId"] in users)
+#     return is_completed and has_max_count
+#
+#
+# with open("filtered_file.json", 'w') as data_file:
+#     filtered_todos = list(filter(keep, todos))
+#     # print(filtered_todos)
+#     json.dump(filtered_todos, data_file, indent=2)
+#
+# with open("filtered_file.json", "r") as fw:
+#     data = json.load(fw)
+#     print(data)
 
-#
-# # print(st1)
-# # st1.add_mark(5)
-# # print(st1)
-# # st1.delete_mark(1)
-# # print(st1)
-# # st1.edit_mark(5, 4)
-# # print(st1)
-# # print(st1.average_mark())
+# CSV
+
+import  csv
+
+with open("data.csv") as r_file:
+    file_reader = csv.reader(r_file, delimiter=";")
+
+    for row in file_reader:
+        print(row)
