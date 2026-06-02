a3f73ef797403f25d5f03c00078a23d58939dc67
MaximBytecamp <mmaxim01@yandex.ru>
2026-06-02 22:06:03 +0300

    02.06.2026

---
+++ parsers.py
@@ -0,0 +1,27 @@
+from bs4 import BeautifulSoup
+import requests
+
+
+class Parser:
+    html = ""
+
+
+    def __init__(self, url, path):
+        self.url = url
+        self.path = path
+
+    def get_html(self):
+        req = requests.get(self.url).text
+        self.html = BeautifulSoup(req, "lxml")
+
+    def parsing(self):
+        news = self.html.find_all("div", class_="caption")
+        for item in news:
+            title = item.find("h3", class_="topic-title").text.strip()
+
+            print(title)
+
+        # print(news)
+    def run(self):
+        self.get_html()
+        self.parsing()
