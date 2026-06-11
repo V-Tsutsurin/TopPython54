dc06ac3bafe68a89c379d693fd27fb1825885dcd
MaximBytecamp <mmaxim01@yandex.ru>
2026-06-11 22:00:19 +0300

    11.06.2026

--- main.py
+++ main.py
@@ -6977,3 +6977,15 @@ import csv
 #
 # if __name__ == "__main__":
 #     run()
+
+import sqlite3 as sq
+
+with sq.connect("profile.db") as con:
+    cur = con.cursor()
+    cur.execute("DROP TABLE users")
+    # cur.execute("""CREATE TABLE IF NOT EXISTS users(
+    # id INTEGER PRIMARY KEY AUTOINCREMENT,
+    # name TEXT NOT NULL,
+    # summa REAL,
+    # date TEXT
+    # )""")
