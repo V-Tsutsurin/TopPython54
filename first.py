8f7214dc82ab1264a48b9eaac1a191d098373482
MaximBytecamp <mmaxim01@yandex.ru>
2026-07-16 22:15:32 +0300

    16.07.2026

--- first.py
+++ first.py
@@ -1,6 +1,29 @@
-from flask import Flask, render_template, url_for, request
+import sqlite3
+import os
+
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort, g
+
+DATABASE = "/tmp/flsk.db"
+DEBUG = True
+SECRET_KEY = '76a57fd9bfc8be917f1763072024a4675cb65084'
+
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(DATABASE=os.path.join(app.root_path, "flsk.db")))
+
+def connect_db():
+    con = sqlite3.connect(app.config['DATABASE'])
+    con.row_factory = sqlite3.Row
+    return con
+
+def create_db():
+    db = connect_db()
+    with app.open_resource("sq_db.sql", mode='r') as f:
+        db.cursor().executescript(f.read())
+    db.commit()
+    db.close()
 
 menu = [
     {"name": "Главная", "url":"index"},
@@ -19,15 +42,54 @@ def about():
     print(url_for("about"))
     return render_template("about.html", title="О нас", menu=menu)
 
+
 @app.route("/profile/<username>")
 def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
     return f"Пользователь: {username}"
 
 @app.route("/contact", methods=["POST", "GET"])
 def contact():
     if request.method == "POST":
-        print(request.form)
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено успешно', category='success')
+        else:
+            flash('Ошибка отправки', category='error')
+
+        # print(request.form)
+        # context = {
+        #     'username': request.form['username'],
+        #     'email': request.form['email'],
+        #     'message': request.form['message']
+        # }
+        # return  render_template("contact.html", **context,title="Обратная связь", menu=menu)
     return render_template("contact.html", title="Обратная связь", menu=menu)
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("page404.html", title="Страница не найдена", menu=menu)
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == "POST" and request.form['username'] == "Viacheslav" and request.form['passw'] == "12345":
+        session['userLogged'] =request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+    return render_template("login.html", title="Авторизация", menu=menu)
+
+
+def get_db():
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+    return g.link_db
+
+@app.teardown_appcontext
+def close_db(error):
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
 
 if __name__ == "__main__":
     app.run(debug=True)
